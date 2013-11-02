2dd08342d1ece58df29a8667ad1e3c1d5de09009
Anton Taraev <anti191@gmail.com>
2013-11-03 02:19:11 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailHackaton::Application.config.secret_key_base = '022f076dd2a020eb75b8637c35a55b5688fe20c63654f9b0f1af73376524161d6ddc309cf82a5e3d7656197cf0a3a47c96e593a39da36b1b4291408239887378'
