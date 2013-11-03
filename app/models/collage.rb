ef158b9def52ae00335e9bfb7582c23668770719
av <av@fun-box.ru>
2013-11-03 16:08:07 +0400

    Collage composing

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -11,12 +11,13 @@ class Collage < ActiveRecord::Base
     images = Image.find(photos.split(','))
     img = []
     images.each do |image|
-      img << Magick::Image.read(image.image.path).first
+      img << Magick::Image.read(image.image.thumb.path).first
     end
-    bg.composite!(img[0], 75, 60, Magick::OverCompositeOp)
-    bg.composite!(img[1], 850, 60, Magick::OverCompositeOp)
-    bg.composite!(img[2], 75, 766, Magick::OverCompositeOp)
-    bg.composite!(img[3], 850, 766, Magick::OverCompositeOp)
+    bg.composite!(img[0], 0, 0, Magick::OverCompositeOp)
+    bg.composite!(img[1], 150, 0, Magick::OverCompositeOp)
+    bg.composite!(img[2], 0, 150, Magick::OverCompositeOp)
+    bg.composite!(img[3], 150, 150, Magick::OverCompositeOp)
+    bg.composite!(img[4], 75, 75, Magick::OverCompositeOp)
     bg.write(file)
     self.image = File.open(file)
   end
