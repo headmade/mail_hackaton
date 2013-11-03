b9a08c25cfdd3bb60ad73bf1c3d072d6b8693c20
Anton Taraev <anti191@gmail.com>
2013-11-03 15:42:46 +0400

    Fix image generate

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -2,12 +2,22 @@ class Collage < ActiveRecord::Base
   mount_uploader :image, CollageUploader
 
   belongs_to :user
+  after_validation :compose
 
   def compose
     require 'RMagick'
+    file = Rails.root.join('tmp', 'col.png')
     bg = Magick::Image.read('app/assets/images/bg.png').first
-    img1 = Magick::Image.read(Image.first.image.path).first
-    bg.composite!(img1, 50, 50, Magick::OverCompositeOp)
-    image = bg
+    images = Image.find(photos.split(','))
+    img = []
+    images.each do |image|
+      img << Magick::Image.read(image.image.path).first
+    end
+    bg.composite!(img[0], 75, 60, Magick::OverCompositeOp)
+    bg.composite!(img[1], 850, 60, Magick::OverCompositeOp)
+    bg.composite!(img[2], 75, 766, Magick::OverCompositeOp)
+    bg.composite!(img[3], 850, 766, Magick::OverCompositeOp)
+    bg.write(file)
+    self.image = File.open(file)
   end
 end
