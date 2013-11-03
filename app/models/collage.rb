c86a4f55f08727c6fe34e20b12cdaba19fcd44fa
av <av@fun-box.ru>
2013-11-03 15:10:43 +0400

    Add composing stuff

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -1,3 +1,13 @@
 class Collage < ActiveRecord::Base
+  mount_uploader :image, CollageUploader
+
   belongs_to :user
+
+  def compose
+    require 'RMagick'
+    bg = Magick::Image.read('app/assets/images/bg.png').first
+    img1 = Magick::Image.read(Image.first.image.path).first
+    bg.composite!(img1, 50, 50, Magick::OverCompositeOp)
+    image = bg
+  end
 end
